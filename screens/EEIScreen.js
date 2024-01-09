e2d04b6a70c8e0c3cc83628b54966d7113d1024a
xdKya <90745676+xdKya@users.noreply.github.com>
2024-01-09 16:04:44 -0300

    Add files via upload

---
+++ screens/EEIScreen.js
@@ -0,0 +1,8 @@
+import react, { Component } from "react";
+import { Text } from "react-native";
+
+export default class EEIScreen extends Component {
+  render() {
+    return <Text>Tela de EEI!</Text>;
+  }
+}
---
+++ screens/meteorScreen.js
@@ -0,0 +1,8 @@
+import react, { Component } from "react";
+import { Text } from "react-native";
+
+export default class MeteorScreen extends Component {
+  render() {
+    return <Text>Tela de meteoros!</Text>;
+  }
+}
